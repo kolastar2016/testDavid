bf3235e37ff985154617ed6645ff23373f7d1b4f
kolastar2016 <kolastarsky@gmail.com>
2018-07-10 03:20:59 +0300

    easy

---
+++ js/build.js
@@ -0,0 +1,2 @@
+$('.carousel').carousel()
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNhcm91c2VsLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIiwiZmlsZSI6ImJ1aWxkLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJCgnLmNhcm91c2VsJykuY2Fyb3VzZWwoKSJdfQ==
